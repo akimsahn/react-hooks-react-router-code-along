5a30f410b6e5a48a67eca738b477a3d850164d3c
Allison Kim <akeem159@yahoo.com>
2022-08-24 23:22:59 -0400

    commit tree 25186998d4ebf1778d473d4383c05540652b4319

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function Home() {
   return (
@@ -9,8 +20,84 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login!</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{background: "darkblue",}}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{background: "darkblue",}}
+      >
+        About
+      </NavLink>  
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{background: "darkblue",}}
+      >
+        Login
+      </NavLink>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
